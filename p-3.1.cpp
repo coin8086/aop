dd2641e98d9b1fc632f0e19b17f59b81bfd4652a
Robert Zhang <louirobert@gmail.com>
2017-06-21 16:16:31 +0800

    Updated solution for problem 3.1.

--- p-3.1.cpp
+++ p-3.1.cpp
@@ -5,10 +5,30 @@
 using namespace std;
 
 bool contain(const string & s1, const string & s2) {
-  if (s1.size() < s2.size())
-    return false;
-  string s3 = s1 + s1;
-  return strstr(s3.c_str(), s2.c_str());
+  //Suppose that i points to the head of a "rotated" string from s1.
+  for (int i = 0; i < s1.size(); i++) {
+    if (s1[i] == s2[0]) {
+      int k = i;
+      int l = 0;
+      while (k < s1.size() && l < s2.size() && s1[k] == s2[l]) {
+        k++;
+        l++;
+      }
+      if (l == s2.size())
+        return true;
+      if (k != s1.size())
+        continue;
+      //Continue comparing with s1 from index 0 to i but not including i.
+      k = 0;
+      while (k < i && l < s2.size() && s1[k] == s2[l]) {
+        k++;
+        l++;
+      }
+      if (l == s2.size())
+        return true;
+    }
+  }
+  return false;
 }
 
 int main() {
